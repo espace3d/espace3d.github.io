e62c8d4232375c8de8cdb74011a5adf58b4befe0
bertrand <espace3d@gmail.com>
2016-10-06 09:01:53 +0200

    this module

--- src/background.js
+++ src/background.js
@@ -1,66 +1,66 @@
 //////////////////////////////////////////////////////////////////////////////////////////
 //background.js
-//e for background
+//this.for background
 //////////////////////////////////////////////////////////////////////////////////////////
 //dessins de tous les éléments du décor
 
 
 function drawBackground(game,group8,group7,group6,group3tris,group3bis,group2,group1) {
-	var e=[]
+	//var this={}
 
 	//////////////////////////////////////////////////////////////////////////////////////////
 	//gray filters
 
-	e.grayfiltertop = game.add.filter('Gray') ; e.grayfiltertop.gray=1
-	e.grayfilternull = game.add.filter('Gray') ; e.grayfilternull.gray=0
+	this.grayfiltertop = game.add.filter('Gray') ; this.grayfiltertop.gray=1
+	this.grayfilternull = game.add.filter('Gray') ; this.grayfilternull.gray=0
 
 	//curseur player lorsque le joueur exerce une pression prolongée
-	e.cursor_player=drawSprite(group3bis,game,"rect",w4*3,h2,w2,w*.1,0.5,red,0)
-	e.cursor_player.flag=true
-	e.cursor_player.isRaise=true	
+	this.cursor_player=drawSprite(group3bis,game,"rect",w4*3,h2,w2,w*.1,0.5,red,0)
+	this.cursor_player.flag=true
+	this.cursor_player.isRaise=true	
 
 	//curseur opponent lorsque le joueur exerce une pression prolongée
-	e.cursor_opponent=drawSprite(group3bis,game,"rect",w4,h2,w2,w*.1,0.5,blue,0)
-	e.cursor_opponent.flag=true
-	e.cursor_opponent.isRaise=true	
+	this.cursor_opponent=drawSprite(group3bis,game,"rect",w4,h2,w2,w*.1,0.5,blue,0)
+	this.cursor_opponent.flag=true
+	this.cursor_opponent.isRaise=true	
 
-	e.cursor_palpitant=drawSprite(group1,game,"cursor_palpitant",w4*3,h2-100,w*.05,w*.05,0.5,0,.4)
-	e.cursor_palpitant.alpha=0
-	e.cursor_palpitant_time=150
+	this.cursor_palpitant=drawSprite(group1,game,"cursor_palpitant",w4*3,h2-100,w*.05,w*.05,0.5,0,.4)
+	this.cursor_palpitant.alpha=0
+	this.cursor_palpitant_time=150
 
-	e.cursor_palpitant_opponent=drawSprite(group1,game,"cursor_palpitant",w4,h2-100,w*.05,w*.05,0.5,0,.4)
-	e.cursor_palpitant_opponent.alpha=0
-	e.cursor_palpitant_time_opponent=150
+	this.cursor_palpitant_opponent=drawSprite(group1,game,"cursor_palpitant",w4,h2-100,w*.05,w*.05,0.5,0,.4)
+	this.cursor_palpitant_opponent.alpha=0
+	this.cursor_palpitant_time_opponent=150
 
-	e.cursor_player_particle = game.add.emitter(e.cursor_player.x, e.cursor_player.y, 200)
-	e.cursor_player_particle.makeParticles("particle_player")
-	e.cursor_player_particle.minParticleSpeed.setTo(-300,-300)
-	e.cursor_player_particle.maxParticleSpeed.setTo(400,400)
-	e.cursor_player_particle.setAlpha(0.5, .9)
+	this.cursor_player_particle = game.add.emitter(this.cursor_player.x, this.cursor_player.y, 200)
+	this.cursor_player_particle.makeParticles("particle_player")
+	this.cursor_player_particle.minParticleSpeed.setTo(-300,-300)
+	this.cursor_player_particle.maxParticleSpeed.setTo(400,400)
+	this.cursor_player_particle.setAlpha(0.5, .9)
 
-	e.cursor_player_particle.minParticleScale = .5
-	e.cursor_player_particle.maxParticleScale = .2
-	e.cursor_player_particle.minRotation = 0
-	e.cursor_player_particle.maxRotation = 0
-	e.cursor_player_particle.on=false
-	e.cursor_player_particle.start(true, 350, 19)
+	this.cursor_player_particle.minParticleScale = .5
+	this.cursor_player_particle.maxParticleScale = .2
+	this.cursor_player_particle.minRotation = 0
+	this.cursor_player_particle.maxRotation = 0
+	this.cursor_player_particle.on=false
+	this.cursor_player_particle.start(true, 350, 19)
 
-	e.cursor_opponent_particle_flag=true
-	e.cursor_opponent_particle = game.add.emitter(e.cursor_opponent.x, e.cursor_opponent.y, 200)
-	e.cursor_opponent_particle.makeParticles("particle_opponent")
-	e.cursor_opponent_particle.minParticleSpeed.setTo(-300,-300)
-	e.cursor_opponent_particle.maxParticleSpeed.setTo(400,400)
-	e.cursor_opponent_particle.setAlpha(0.5, .9)
+	this.cursor_opponent_particle_flag=true
+	this.cursor_opponent_particle = game.add.emitter(this.cursor_opponent.x, this.cursor_opponent.y, 200)
+	this.cursor_opponent_particle.makeParticles("particle_opponent")
+	this.cursor_opponent_particle.minParticleSpeed.setTo(-300,-300)
+	this.cursor_opponent_particle.maxParticleSpeed.setTo(400,400)
+	this.cursor_opponent_particle.setAlpha(0.5, .9)
 
-	e.cursor_opponent_particle.minParticleScale = .5
-	e.cursor_opponent_particle.maxParticleScale = .2
-	e.cursor_opponent_particle.minRotation = 0
-	e.cursor_opponent_particle.maxRotation = 0
-	e.cursor_opponent_particle.on=false
-	e.cursor_opponent_particle.start(true, 350, 19)
+	this.cursor_opponent_particle.minParticleScale = .5
+	this.cursor_opponent_particle.maxParticleScale = .2
+	this.cursor_opponent_particle.minRotation = 0
+	this.cursor_opponent_particle.maxRotation = 0
+	this.cursor_opponent_particle.on=false
+	this.cursor_opponent_particle.start(true, 350, 19)
 
 	//à 0 pour ne pas le voir au début
-	e.fond=drawSprite(group8,game,"back",0,0,w,h,0,0,1) 
+	this.fond=drawSprite(group8,game,"back",0,0,w,h,0,0,1) 
 
 	//épaisseur des bordures pour le menu avec les différents papiers
 	var epaisseur_fond=25
@@ -69,44 +69,44 @@ function drawBackground(game,group8,group7,group6,group3tris,group3bis,group2,gr
 	var decalage=epaisseur_fond*.1	
 
 	//différents fond pour le cadre constituant le menu de sélection des papiers
-	e.border_player_gauche=drawSprite(group8,game,"rect",w2+decalage,0,epaisseur_fond,h,0,blue,1) 
-	e.border_player_droit=drawSprite(group8,game,"rect",w-epaisseur_fond,0,epaisseur_fond,h,0,blue,1) 
-	e.border_player_superieur=drawSprite(group8,game,"rect",w2+epaisseur_fond,0,w2,epaisseur_fond,0,blue,1) 
-	e.border_player_inferieur=drawSprite(group8,game,"rect",w2+epaisseur_fond,h-epaisseur_fond_large,w2,epaisseur_fond_large,0,blue,1) 
+	this.border_player_gauche=drawSprite(group8,game,"rect",w2+decalage,0,epaisseur_fond,h,0,blue,1) 
+	this.border_player_droit=drawSprite(group8,game,"rect",w-epaisseur_fond,0,epaisseur_fond,h,0,blue,1) 
+	this.border_player_superieur=drawSprite(group8,game,"rect",w2+epaisseur_fond,0,w2,epaisseur_fond,0,blue,1) 
+	this.border_player_inferieur=drawSprite(group8,game,"rect",w2+epaisseur_fond,h-epaisseur_fond_large,w2,epaisseur_fond_large,0,blue,1) 
 
-	e.border_opponent_gauche=drawSprite(group8,game,"rect",0,0,epaisseur_fond,h,0,red,1) 
-	e.border_opponent_droit=drawSprite(group8,game,"rect",w2-(epaisseur_fond+decalage),0,epaisseur_fond,h,0,red,1) 
-	e.border_opponent_superieur=drawSprite(group8,game,"rect",0-epaisseur_fond,0,w2,epaisseur_fond,0,red,1) 
-	e.border_opponent_inferieur=drawSprite(group8,game,"rect",0-epaisseur_fond,h-epaisseur_fond_large,w2,epaisseur_fond_large,0,red,1) 
+	this.border_opponent_gauche=drawSprite(group8,game,"rect",0,0,epaisseur_fond,h,0,red,1) 
+	this.border_opponent_droit=drawSprite(group8,game,"rect",w2-(epaisseur_fond+decalage),0,epaisseur_fond,h,0,red,1) 
+	this.border_opponent_superieur=drawSprite(group8,game,"rect",0-epaisseur_fond,0,w2,epaisseur_fond,0,red,1) 
+	this.border_opponent_inferieur=drawSprite(group8,game,"rect",0-epaisseur_fond,h-epaisseur_fond_large,w2,epaisseur_fond_large,0,red,1) 
 
-	//background de l'opponent et du player
-	e.opponent=drawSprite(group6,game,"rect",0-decalage,0,w2,h,0,red,1) 
-	e.player=drawSprite(group7,game,"rect",w2+decalage,0,w2,h,0,blue,1) 
-	e.player.inputEnabled=true
+	//this.background de l'opponent et du player
+	this.opponent=drawSprite(group6,game,"rect",0-decalage,0,w2,h,0,red,1) 
+	this.player=drawSprite(group7,game,"rect",w2+decalage,0,w2,h,0,blue,1) 
+	this.player.inputEnabled=true
 
 	//ombre pour symboliser la table
-	e.table_player=drawSprite(group3tris,game,"rect",w2,h2,w2,h2,0,black,.2) 
-	e.table_opponent=drawSprite(group3tris,game,"rect",0,h2,w2,h2,0,black,.2) 
+	this.table_player=drawSprite(group3tris,game,"rect",w2,h2,w2,h2,0,black,.2) 
+	this.table_opponent=drawSprite(group3tris,game,"rect",0,h2,w2,h2,0,black,.2) 
 
 	//lignes à traits tirés pour symboliser la chute imminente
 	var longeur_line=w/6
-	e.line_player_droite=drawSprite(group7,game,"line",w/6*5,h2,longeur_line,w*.01,0,0,1)
-	e.line_opponent_gauche=drawSprite(group6,game,"line",0,h2,longeur_line,w*.01,0,0,1)
-	e.line_player_droite.alpha=0
-	e.line_opponent_gauche.alpha=0
+	this.line_player_droite=drawSprite(group7,game,"line",w/6*5,h2,longeur_line,w*.01,0,0,1)
+	this.line_opponent_gauche=drawSprite(group6,game,"line",0,h2,longeur_line,w*.01,0,0,1)
+	this.line_player_droite.alpha=0
+	this.line_opponent_gauche.alpha=0
 
 	//barre supérieure qui masque la chute du papier-- groupe différent pour que le texte et le papier soit recouvert 
-	e.opponent_top=drawSprite(group1,game,"rect",0-decalage,0,w2,h*.15,0,red,1) 
-	e.player_top=drawSprite(group2,game,"rect",w2+decalage,0,w2,h*.15,0,blue,1) 
+	this.opponent_top=drawSprite(group1,game,"rect",0-decalage,0,w2,h*.15,0,red,1) 
+	this.player_top=drawSprite(group2,game,"rect",w2+decalage,0,w2,h*.15,0,blue,1) 
 
 	//line de collision avec le paper opponent
-	e.line_collision_opponent=[]
+	this.line_collision_opponent=[]
 
 	for (var j = 0; j < 5; j++) {
-		e.line_collision_opponent[j]=game.add.sprite(0,game.rnd.integerInRange(h2,(h+h2)),"line_collision")
-		e.line_collision_opponent[j].isTouch=false
+		this.line_collision_opponent[j]=game.add.sprite(0,game.rnd.integerInRange(h2,(h+h2)),"line_collision")
+		this.line_collision_opponent[j].isTouch=false
 
-		e.line_collision_opponent[j].alpha=.1
+		this.line_collision_opponent[j].alpha=.1
 	}
 
 	//textes du player et de l'opponent
@@ -119,108 +119,108 @@ function drawBackground(game,group8,group7,group6,group3tris,group3bis,group2,gr
 
 	//texte symbolisant l'ombre sous le player et dont la visibilité apparait dans update via the Game.js
 	//player
-	e.text_name_player_shadow = game.add.bitmapText(w4*3+3,py1+3,'lucky','dev', taille) 
-	e.text_name_player_shadow.alpha=.5
-	e.text_name_player_shadow.visible=false
-	e.text_name_player = game.add.bitmapText(w4*3,py1,'lucky','dev', taille) 
-	e.text_level_player = game.add.bitmapText(w4*3,py3,'lucky','lvl ', taille2) 
-	e.text_level_player.alpha=0
-	e.text_level_number_player=game.add.bitmapText(w4*3+30,py3,'lucky','1', taille2) 
-	e.text_level_number_player.tint=jaune
-	e.text_level_number_player.alpha=0
+	this.text_name_player_shadow = game.add.bitmapText(w4*3+3,py1+3,'lucky','dev', taille) 
+	this.text_name_player_shadow.alpha=.5
+	this.text_name_player_shadow.visible=false
+	this.text_name_player = game.add.bitmapText(w4*3,py1,'lucky','dev', taille) 
+	this.text_level_player = game.add.bitmapText(w4*3,py3,'lucky','lvl ', taille2) 
+	this.text_level_player.alpha=0
+	this.text_level_number_player=game.add.bitmapText(w4*3+30,py3,'lucky','1', taille2) 
+	this.text_level_number_player.tint=jaune
+	this.text_level_number_player.alpha=0
 
-	e.text_win_player = game.add.bitmapText(w4*3,h2,'lucky','win', taille) 
-	e.text_win_player.tint=jaune
-	e.text_win_player.visible=false
+	this.text_win_player = game.add.bitmapText(w4*3,h2,'lucky','win', taille) 
+	this.text_win_player.tint=jaune
+	this.text_win_player.visible=false
 
-	e.text_loose_player = game.add.bitmapText(w4*3,h2,'lucky','loose', taille) 
-	e.text_loose_player.tint=jaune
-	e.text_loose_player.visible=false
+	this.text_loose_player = game.add.bitmapText(w4*3,h2,'lucky','loose', taille) 
+	this.text_loose_player.tint=jaune
+	this.text_loose_player.visible=false
 
 	//OPPONENT 
-	e.text_name_opponent = game.add.bitmapText(w4,py1,'lucky','kill the game', taille) 
-	e.text_level_opponent = game.add.bitmapText(w4,py3,'lucky','lvl ', taille2) 
-	e.text_level_opponent.alpha=0
-	e.text_level_number_opponent=game.add.bitmapText(w4+30,py3,'lucky','5', taille2) 
-	e.text_level_number_opponent.tint=jaune
-	e.text_level_number_opponent.alpha=0
+	this.text_name_opponent = game.add.bitmapText(w4,py1,'lucky','kill the game', taille) 
+	this.text_level_opponent = game.add.bitmapText(w4,py3,'lucky','lvl ', taille2) 
+	this.text_level_opponent.alpha=0
+	this.text_level_number_opponent=game.add.bitmapText(w4+30,py3,'lucky','5', taille2) 
+	this.text_level_number_opponent.tint=jaune
+	this.text_level_number_opponent.alpha=0
 
-	e.text_win_opponent = game.add.bitmapText(w4,h2,'lucky','win', taille) 
-	e.text_win_opponent.tint=jaune
-	e.text_win_opponent.visible=false
+	this.text_win_opponent = game.add.bitmapText(w4,h2,'lucky','win', taille) 
+	this.text_win_opponent.tint=jaune
+	this.text_win_opponent.visible=false
 
-	e.text_loose_opponent = game.add.bitmapText(w4,h2,'lucky','loose', taille) 
-	e.text_loose_opponent.tint=jaune
-	e.text_loose_opponent.visible=false
+	this.text_loose_opponent = game.add.bitmapText(w4,h2,'lucky','loose', taille) 
+	this.text_loose_opponent.tint=jaune
+	this.text_loose_opponent.visible=false
 
 	//modifications des anchors
-	e.text_name_player.anchor.x=.5
-	e.text_name_player.anchor.y=.5
-	e.text_name_player_shadow.anchor.x=.5
-	e.text_name_player_shadow.anchor.y=.5
-	e.text_level_player.anchor.x=.5
-	e.text_level_player.anchor.y=.5
-	e.text_level_number_player.anchor.x=.5
-	e.text_level_number_player.anchor.y=.5
-	e.text_win_player.anchor.x=.5
-	e.text_win_player.anchor.y=.5
-	e.text_loose_player.anchor.x=.5
-	e.text_loose_player.anchor.y=.5
+	this.text_name_player.anchor.x=.5
+	this.text_name_player.anchor.y=.5
+	this.text_name_player_shadow.anchor.x=.5
+	this.text_name_player_shadow.anchor.y=.5
+	this.text_level_player.anchor.x=.5
+	this.text_level_player.anchor.y=.5
+	this.text_level_number_player.anchor.x=.5
+	this.text_level_number_player.anchor.y=.5
+	this.text_win_player.anchor.x=.5
+	this.text_win_player.anchor.y=.5
+	this.text_loose_player.anchor.x=.5
+	this.text_loose_player.anchor.y=.5
 
-	e.text_name_opponent.anchor.x=.5
-	e.text_name_opponent.anchor.y=.5
-	e.text_level_opponent.anchor.x=.5
-	e.text_level_opponent.anchor.y=.5
-	e.text_level_number_opponent.anchor.x=.5
-	e.text_level_number_opponent.anchor.y=.5
-	e.text_win_opponent.anchor.x=.5
-	e.text_win_opponent.anchor.y=.5
-	e.text_loose_opponent.anchor.x=.5
-	e.text_loose_opponent.anchor.y=.5
+	this.text_name_opponent.anchor.x=.5
+	this.text_name_opponent.anchor.y=.5
+	this.text_level_opponent.anchor.x=.5
+	this.text_level_opponent.anchor.y=.5
+	this.text_level_number_opponent.anchor.x=.5
+	this.text_level_number_opponent.anchor.y=.5
+	this.text_win_opponent.anchor.x=.5
+	this.text_win_opponent.anchor.y=.5
+	this.text_loose_opponent.anchor.x=.5
+	this.text_loose_opponent.anchor.y=.5
 
 	//ajout des textes aux groupes
-	group2.add(e.text_name_player) 
-	group2.add(e.text_name_player_shadow) 
-	group2.add(e.text_level_player) 
-	group2.add(e.text_level_number_player) 
+	group2.add(this.text_name_player) 
+	group2.add(this.text_name_player_shadow) 
+	group2.add(this.text_level_player) 
+	group2.add(this.text_level_number_player) 
 
-	group1.add(e.text_level_opponent) 
-	group1.add(e.text_name_opponent) 
-	group1.add(e.text_level_number_opponent) 
+	group1.add(this.text_level_opponent) 
+	group1.add(this.text_name_opponent) 
+	group1.add(this.text_level_number_opponent) 
 
 	//////////////////////////////////////////////////////////////////////////////////////////
 	//TRANSITIONS
 	//ligne à traits tirés apparaissant pour signifier le mileu de la table
-	e.tween_level_and_number_player1=game.add.tween(e.text_level_player).to({alpha:1},500,Phaser.Easing.Linear.None,true,delay_paper_fall)
-	e.tween_level_and_number_player2=game.add.tween(e.text_level_number_player).to({alpha:1},500,Phaser.Easing.Linear.None,true,delay_paper_fall)
-	e.tween_level_and_number_opponent1=game.add.tween(e.text_level_opponent).to({alpha:1},500,Phaser.Easing.Linear.None,true,delay_paper_fall)
-	e.tween_level_and_number_opponent2=game.add.tween(e.text_level_number_opponent).to({alpha:1},500,Phaser.Easing.Linear.None,true,delay_paper_fall)
-	e.tween_line_opponent_appears=game.add.tween(e.line_opponent_gauche).to({alpha:1},500,Phaser.Easing.Linear.None,true,3500)
-	e.tween_line_player_appears=game.add.tween(e.line_player_droite).to({alpha:1},500,Phaser.Easing.Linear.None,true,3500)
-	e.tween_cursor_appears=game.add.tween(e.cursor_palpitant).to({alpha:.6},1500,Phaser.Easing.Linear.None,true,3500)
-	e.tween_cursor_appears_opponent=game.add.tween(e.cursor_palpitant_opponent).to({alpha:.6},1500,Phaser.Easing.Linear.None,true,3500)
-	e.cursor_palpitant_tween=game.add.tween(e.cursor_palpitant.scale).to({x:1.2, y:1.2},e.cursor_palpitant_time,Phaser.Easing.Sinusoidal.In,true,5500,-1,false)
-	e.cursor_palpitant_tween_opponent=game.add.tween(e.cursor_palpitant_opponent.scale).to({x:1.2, y:1.2},e.cursor_palpitant_time,Phaser.Easing.Sinusoidal.In,true,5500,-1,false)
+	this.tween_level_and_number_player1=game.add.tween(this.text_level_player).to({alpha:1},500,Phaser.Easing.Linear.None,true,delay_paper_fall)
+	this.tween_level_and_number_player2=game.add.tween(this.text_level_number_player).to({alpha:1},500,Phaser.Easing.Linear.None,true,delay_paper_fall)
+	this.tween_level_and_number_opponent1=game.add.tween(this.text_level_opponent).to({alpha:1},500,Phaser.Easing.Linear.None,true,delay_paper_fall)
+	this.tween_level_and_number_opponent2=game.add.tween(this.text_level_number_opponent).to({alpha:1},500,Phaser.Easing.Linear.None,true,delay_paper_fall)
+	this.tween_line_opponent_appears=game.add.tween(this.line_opponent_gauche).to({alpha:1},500,Phaser.Easing.Linear.None,true,3500)
+	this.tween_line_player_appears=game.add.tween(this.line_player_droite).to({alpha:1},500,Phaser.Easing.Linear.None,true,3500)
+	this.tween_cursor_appears=game.add.tween(this.cursor_palpitant).to({alpha:.6},1500,Phaser.Easing.Linear.None,true,3500)
+	this.tween_cursor_appears_opponent=game.add.tween(this.cursor_palpitant_opponent).to({alpha:.6},1500,Phaser.Easing.Linear.None,true,3500)
+	this.cursor_palpitant_tween=game.add.tween(this.cursor_palpitant.scale).to({x:1.2, y:1.2},this.cursor_palpitant_time,Phaser.Easing.Sinusoidal.In,true,5500,-1,false)
+	this.cursor_palpitant_tween_opponent=game.add.tween(this.cursor_palpitant_opponent.scale).to({x:1.2, y:1.2},this.cursor_palpitant_time,Phaser.Easing.Sinusoidal.In,true,5500,-1,false)
 	//////////////////////////////////////////////////////////////////////////////////////////
 	//animation du texte du joueur lorsqu'on maintient la pression appuyée
-	e.panimTween=game.add.tween(e.text_name_player.scale).to({x:2, y:2},150,Phaser.Easing.Sinusoidal.In,true,0,-1,true)
-	e.panimTween_shadow=game.add.tween(e.text_name_player_shadow.scale).to({x:2.1, y:2.1},155,Phaser.Easing.Sinusoidal.In,true,0,-1,true)
-	e.panimTween.pause()
-	e.panimTween_shadow.pause()
+	this.panimTween=game.add.tween(this.text_name_player.scale).to({x:2, y:2},150,Phaser.Easing.Sinusoidal.In,true,0,-1,true)
+	this.panimTween_shadow=game.add.tween(this.text_name_player_shadow.scale).to({x:2.1, y:2.1},155,Phaser.Easing.Sinusoidal.In,true,0,-1,true)
+	this.panimTween.pause()
+	this.panimTween_shadow.pause()
 
 //	e.check_winner=function(stoptween,fonction,obj){
 //		console.log("stoptween")
-//		//if (e.text_win_player && e.text_loose_opponent) {
+//		//if (this.text_win_player && this.text_loose_opponent) {
 //			//stoptween
 //			//fonction
 //		//}
-//		//else if (e.text_loose_player && e.text_win_opponent){
+//		//else if (this.text_loose_player && this.text_win_opponent){
 //			stoptween
 //			fonction(obj)
 //		//}
 //	}
 
-	return e
+	return this
 }
 
 //////////////////////////////////////////////////////////////////////////////////////////
